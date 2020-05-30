db5a0c5e19f4b77c64f2be8a9159d68c896c717a
Josh Fogus <josh.fogus@gmail.com>
2020-05-30 14:20:16 -0700

    SquareView Completed.

--- views/SquareView.py
+++ views/SquareView.py
@@ -10,11 +10,13 @@ from PySide2.QtGui import QPixmap, QMouseEvent
 
 
 class SquareView(QLabel):
-    """ Represents a square on a Gess board. """
-    # TODO: Move stones from instance variable to class variable
+    """ Represents a square on a Gess board.  Accepts a tuple indicating
+        the squares coordinates within the Gess board. """
+    clicked = Signal(tuple)
 
-    def __init__(self):
+    def __init__(self, coords):
         super(SquareView, self).__init__()
+        self._coords = coords
 
         # Set default appearance
         self.setFixedSize(20, 20)
@@ -23,44 +25,50 @@ class SquareView(QLabel):
 
         # Set styles
         # TODO: Factor styles into a separate file.
-        # 00: default; 01: center; 10: peripheral
-        self.styles = {
+        self._styles = {
             "default": "border: 1px solid #A9A9A9;",
             "center": "border: 1px solid #221CD9;",
             "peripheral": "border: 1px solid #1BCF6C;"
         }
 
-        self.setStyleSheet(self.styles["default"])
+        self.setStyleSheet(self._styles["default"])
 
+        # TODO: Move stones from instance variable to class variable
         # Load stone images
-        self.stones = {
+        self._stones = {
             'w': QPixmap("assets/white_circle.png"),
             'b': QPixmap("assets/black_circle.png")
         }
 
-    def on_click(self):
-        """ Emits a click event. """
-        Signal("")
+    def mousePressEvent(self, event: QMouseEvent):
+        """ Emits a signal on mouse press with the row, col coordinates
+            of the clicked square. """
+        self.clicked.emit(self._coords)
+
+    def get_coords(self):
+        """ Returns the coordinates of the square as set when
+            initialized. """
+        return self._coords
 
     def highlight_as_center(self):
         """ Changes the color of a cell to indicate it as the center
             of a piece. """
-        self.setStyleSheet(self.styles["center"])
+        self.setStyleSheet(self._styles["center"])
 
     def highlight_as_peripheral(self):
         """ Changes the color of a cell to indicate it as a peripheral
             member of a piece. """
-        self.setStyleSheet(self.styles["peripheral"])
+        self.setStyleSheet(self._styles["peripheral"])
 
     def remove_highlight(self):
         """ Changes the color of the cell to its default color. """
-        self.setStyleSheet(self.styles["default"])
+        self.setStyleSheet(self._styles["default"])
 
     def place_stone(self, stone):
         """ Receives a string indicating what color stone to place.
             'b' or 'w' Places a stone with the corresponding color
-             in the square. """
-        self.setPixmap(self.stones[stone])
+            in the square. """
+        self.setPixmap(self._stones[stone])
 
     def remove_stone(self):
         """ Clears the square of any stone. """
