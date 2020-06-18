e192dd4a903b7a2f32263b9d25381291cc0bdf05
Josh Fogus <josh.fogus@gmail.com>
2020-06-17 18:11:38 -0700

    Now handles instantiating and passing objects

--- Gess.py
+++ Gess.py
@@ -5,10 +5,16 @@
 
 import sys
 from models.Game import Game
+from models.Player import Player
+from models.Board import Board
 from models.History import History
 from controllers.BoardController import BoardController
 from controllers.HistoryController import HistoryController
 from views.GameView import GameView
+from views.BoardView import BoardView
+from views.SquareView import SquareView
+from views.StatusView import StatusView
+from views.HistoryView import HistoryView
 from PySide2.QtWidgets import QApplication
 
 
@@ -18,18 +24,24 @@ class Gess(QApplication):
         super(Gess, self).__init__(sys_argv)
 
         # Models
-        self._game_model = Game()
-        self._history_model = History()
+        self._board = Board()
+        # TODO: Move to external file of constants
+        self._players = (Player('b'), Player('w'))
+        self._game = Game(self._players, self._board)
+        self._history = History(self._game)
 
         # Controllers
-        self._board_controller = BoardController(self._game_model)
-        self._history_controller = HistoryController(self._game_model)
+        self._board_controller = BoardController(self._game)
+        self._history_controller = HistoryController(self._game)
+
+        board_size = len(self._board.get_squares())
 
         # Views
-        self._game_view = GameView(self._game_model,
-                                  self._board_controller,
-                                  self._history_model,
-                                  self._history_controller)
+        self._square_views = [[SquareView((i, j)) for j in range(board_size)] for i in range(board_size)]
+        self._board_view = BoardView(self._square_views, self._game, self._board_controller)
+        self._status_view = StatusView(self._game)
+        self._history_view = HistoryView(self._history, self._history_controller)
+        self._game_view = GameView(self._board_view, self._status_view, self._history_view)
         self._game_view.show()
 
 
